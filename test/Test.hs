cdc73f1c533b0682bad5cc139b2f70ceec2638f6
Samuel Schlesinger <samuel@simspace.com>
2021-09-17 02:31:09 -0400

    Semigroups galore

--- test/Test.hs
+++ test/Test.hs
@@ -12,11 +12,15 @@ requires msg = mapM_ (uncurry go) . zip [1..] where
 
 main :: IO ()
 main = do
-  let merge = (,) <$> optional fst <*> required snd
+  let merge = (,,) <$> optional (\(x,_,_) -> x) <*> required (\(_,x,_) -> x) <*> combine (\(_,_,x) -> x)
+  let merge' = combine Max
+  let merge'' = combine Last
   requires "merge"
-    [ runMerge merge (Just 10, 1) (Nothing, 1) == Just (Just 10, 1) 
-    , runMerge merge (Nothing, 1) (Nothing, 1) == Just (Nothing, 1)
-    , runMerge merge (Nothing, 1) (Nothing, 2) == Nothing
-    , runMerge merge (Just 10, 1) (Just 11, 1) == Nothing
-    , runMerge merge (Just 10, 1) (Just 11, 2) == Nothing
+    [ runMerge merge (Just 10, 1, []) (Nothing, 1, [1]) == Just (Just 10, 1, [1]) 
+    , runMerge merge (Nothing, 1, [2]) (Nothing, 1, [3]) == Just (Nothing, 1, [2, 3])
+    , runMerge merge (Nothing, 1, [1, 2]) (Nothing, 2, [3, 4]) == Nothing
+    , runMerge merge (Just 10, 1, [7]) (Just 11, 1, []) == Nothing
+    , runMerge merge (Just 10, 1, []) (Just 11, 2, []) == Nothing
+    , runMerge merge' 5 10 == Just (Max 10)
+    , runMerge merge'' True False == Just (Last False)
     ]
